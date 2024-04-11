18fac8d63dafe75e10dd16d55e2be4b6fdba521d
davidkrajncc <david.krajnc4@student.um.si>
2024-04-11 16:42:29 +0200

    poskus docker 4

--- .github/sh/zapakiraj_27114.sh
+++ .github/sh/zapakiraj_27114.sh
@@ -7,5 +7,5 @@ docker build -t your-image-name .
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 # Potisk slike v DockerHub
-docker tag your-image-name "${DOCKER_USERNAME}/${REPO_NAME}:${GITHUB_SHA}"
-docker push "${DOCKER_USERNAME}/${REPO_NAME}:${GITHUB_SHA}"
+docker tag your-image-name "${DOCKER_USERNAME}/${REPO_NAME}:latest"
+docker push "${DOCKER_USERNAME}/${REPO_NAME}:latest"
