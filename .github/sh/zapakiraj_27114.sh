01846ea68029b034c807f715b70ed7cba61d9a3f
davidkrajncc <david.krajnc4@student.um.si>
2024-04-13 21:21:58 +0200

    zadnni poskus pred oddajo

--- .github/sh/zapakiraj_27114.sh
+++ .github/sh/zapakiraj_27114.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-# Gradnja Docker slike
 docker build -t davidkrajncc/sa_vaja3in4_docker:latest
 
-# Prijava v DockerHub
 echo "${DOCKERHUB_PASSWORD}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
 
-# Potisk slike v DockerHub
 docker tag your-image-name "${DOCKERHUB_USERNAME}/${REPO_NAME}:latest"
 docker push "${DOCKERHUB_USERNAME}/${REPO_NAME}:latest"
